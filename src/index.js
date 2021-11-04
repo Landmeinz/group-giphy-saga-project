8298328d983760ff94bdfd0cfdca7a3c99ec53ef
John Gaffaney <87045628+jgaffaney@users.noreply.github.com>
2021-11-04 10:36:55 -0500

    results reducer done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import axios from 'axios';
+
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react';
+import logger from 'redux-logger';
+import createSageMiddleware from 'redux-saga';
+import { takeEvery, put } from 'redux-saga/effects';
+
+const results = (state = [], action) => {
+    switch(action.type) {
+        case 'SET_RESULTS':
+            return action.payload
+        default:
+            return state;
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
