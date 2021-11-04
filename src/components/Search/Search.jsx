e4c8419f6d079219c5810d463dd3f95a03ce10ae
Eric Meinzer <emeinzer7@gmail.com>
2021-11-04 10:59:26 -0500

    search and search from components set up; buttons and inputs placed in search form

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,13 @@
+
+import SearchForm from '../SearchForm/SearchForm.jsx'
+
+function Search() {
+    return(
+        <div>
+            <h1>Giphy Search!</h1>
+            <SearchForm />
+        </div>
+    )
+};
+
+export default Search; 
