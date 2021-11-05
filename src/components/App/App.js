6679d72d720850c24516f813dea5562765fb1909
Matt Dow <matt.dow.vo@gmail.com>
2021-11-05 09:59:50 -0500

    Debugged navbar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,13 +3,17 @@ import Favorites from '../Favorites/Favorites.jsx';
 import Search from '../Search/Search.jsx';
 import Navbar from '../Navbar/Navbar.jsx';
 import { HashRouter as Router, Route } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 
 function App(props) {
+  const history = useHistory();
+  
+  
   return (
     <>
-      <Navbar />
       <Router>
+        <Navbar />
         <Route exact path='/'>
           <Search />
         </Route>
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,29 @@
 import React from 'react';
 import { Button } from '@mui/material';
 import { Typography } from '@mui/material';
+import { useHistory } from 'react-router-dom';
+import './navbar.css'
 
 function Navbar() {
+    const history = useHistory();
     return (
-        <nav>
+        <nav className="nav-bar">
             <Typography 
                 sx = {{m:2, display:'inline-block'}}
                 variant='h4'>GIPHY SEARCH</Typography>
             <Button
                 sx = { {m:2, display:'inline-block'} }
+                size = "large"
+                onClick={(event) => {history.push('/')}}
             >HOME</Button>
             <Button
-                sx = { {m:2, display:'inline-block'} }>FAVORITES</Button>
-
-
-
+                sx = { {m:2, display:'inline-block'} }
+                size = "large"
+                onClick={(event) => {
+                    console.log(history);
+                    history.push('/favorites')}}
+            >FAVORITES</Button>
         </nav>
     )
-
 } 
-export default Navbar;git
+export default Navbar;
