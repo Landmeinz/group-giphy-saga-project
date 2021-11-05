48db2141a8422391f35f548c7316e706a6179d75
Matt Dow <matt.dow.vo@gmail.com>
2021-11-05 09:29:13 -0500

    Started to add routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import Search from '../Search/Search.jsx';
 import Navbar from '../Navbar/Navbar.jsx';
+import { Route, HashRouter as Router, Link} from 'react-router-dom';
 
 function App(props) {
   return (
     <>
-    <Navbar />
+      <Navbar />
+    
+      <Router>
+        <Route path="/" exact>
+          <Search />
+        </Route>
+        <Route path="/favorites">
+          <
+        </Route>
+
+
+      </Router>
+    
     <div>
-      <Search />
+      
     </div>
     </>
   );
