2a78634e51dae172f1aab259f98e287ea8ae4d96
Eric Meinzer <emeinzer7@gmail.com>
2021-11-05 10:01:40 -0500

    need new master

--- src/components/SearchList/SearchList.jsx
+++ src/components/SearchList/SearchList.jsx
@@ -1,6 +1,13 @@
 import { useSelector } from 'react-redux';
-
 import ListItem from '../ListItem/ListItem.jsx'
+
+// import MUI components;
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 // import dataList from './dataList.js'
 
@@ -14,14 +21,14 @@ function SearchList(props) {
 
     // const gifList = dataList
     return(
-        <div>
+        <Card sx={{ minWidth: 50 }}>
             {gifList.map(gif => (
                 <ListItem 
                     key={gif.id}
                     gif={gif}
                 />
             ))}
-        </div>
+        </Card>
     )
 };
 
